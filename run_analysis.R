76627f3c38b2348407ec2c0f4ed6873bef715dc0
KeyCruncher <troelskchristensen@gmail.com>
2016-06-25 14:22:22 +0200

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -2,7 +2,7 @@ library(reshape2)
 
 fileName <- "getdata_dataset.zip"
 
-# Downloads and unzips the dataset:
+#Downloads and unzips the dataset:
 if (!file.exists(fileName))
 {
   fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip "
@@ -14,13 +14,13 @@ if (!file.exists("UCI HAR Dataset"))
   unzip(fileName) 
 }
 
-# Loads activity labels and features
+#Loads activity labels and features
 actLabels <- read.table("UCI HAR Dataset/activity_labels.txt")
 actLabels[,2] <- as.character(actLabels[,2])
 features <- read.table("UCI HAR Dataset/features.txt")
 features[,2] <- as.character(features[,2])
 
-# Extract the data on mean and standard deviation
+#Extract the data on mean and standard deviation
 featuresWanted <- grep(".*mean.*|.*std.*", features[,2])
 featuresWanted.names <- features[featuresWanted,2]
 featuresWanted.names = gsub('-mean', 'Mean', featuresWanted.names)
@@ -28,7 +28,7 @@ featuresWanted.names = gsub('-std', 'Std', featuresWanted.names)
 featuresWanted.names <- gsub('[-()]', '', featuresWanted.names)
 
 
-# Loads the datasets
+#Loads the datasets
 train <- read.table("UCI HAR Dataset/train/X_train.txt")[featuresWanted]
 trainActivities <- read.table("UCI HAR Dataset/train/Y_train.txt")
 trainSubjects <- read.table("UCI HAR Dataset/train/subject_train.txt")
@@ -38,14 +38,15 @@ testActivities <- read.table("UCI HAR Dataset/test/Y_test.txt")
 testSubjects <- read.table("UCI HAR Dataset/test/subject_test.txt")
 test <- cbind(testSubjects, testActivities, test)
 
-# Merges datasets and adds labels
+#Merges datasets and adds labels
 allData <- rbind(train, test)
 colnames(allData) <- c("subject", "activity", featuresWanted.names)
 
-# Turns activities and subjects into factors
+#Turns activities and subjects into factors
 allData$activity <- factor(allData$activity, levels = activityLabels[,1], labels = activityLabels[,2])
 allData$subject <- as.factor(allData$subject)
 allData.melted <- melt(allData, id = c("subject", "activity"))
 allData.mean <- dcast(allData.melted, subject + activity ~ variable, mean)
 
+#Saves the data to the tidyDataSet file
 write.table(allData.mean, "tidyDataSet.txt", row.names = FALSE, quote = FALSE)
