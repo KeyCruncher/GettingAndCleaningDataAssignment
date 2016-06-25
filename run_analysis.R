786ffc8f289cf96983e9ce2967e69369be28a411
KeyCruncher <troelskchristensen@gmail.com>
2016-06-25 14:20:10 +0200

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -2,7 +2,7 @@ library(reshape2)
 
 fileName <- "getdata_dataset.zip"
 
-## Downloads and unzips the dataset:
+# Downloads and unzips the dataset:
 if (!file.exists(fileName))
 {
   fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip "
